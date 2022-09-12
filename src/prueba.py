b7e7ea1e0efe130f3bda3dcaec1389a38c051c5f
Victor Torres <victor.torrest@ciencias.unam.mx>
2022-09-11 23:53:07 -0500

    Añadiendo las funcionalidades de la cache, json para implementarlo en la interfaz

--- src/prueba.py
+++ src/prueba.py
@@ -1,14 +1,15 @@
 import urllib.request
 import urllib.parse
 import http.client
-import numpy
 import json
 import csv
+from datetime import datetime
+from datetime import timedelta
 
-class ComplexEncoder(json.JSONEncoder):
+class CiudadesEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, Ciudades):
-            return [obj.nombre, obj.longitud, obj.latitud]
+            return [obj.nombre, obj.latitud, obj.longitud]
         # Let the base class default method raise the TypeError
         return json.JSONEncoder.default(self, obj)
 class Ciudades:
@@ -21,83 +22,117 @@ class Ciudades:
     def __repr__(self):
         return "[ " + self.nombre + ", " + str(self.latitud) + ", " + str(self.longitud) + "]"
     
-def funcionHash(vuelo, n):
+def funcionHash(ciudad, n):
     suma = 0
-    for caracter in vuelo[0]:
+    for caracter in ciudad.nombre:
         suma += ord(caracter)
-    return (suma + abs(int(float(vuelo[2]))) + abs(int(float(vuelo[3])))) % n
-def buscarCiudadOrigen(listaCiudades, vuelo, n):
+    return (suma + abs(int(float(ciudad.latitud))) + abs(int(float(ciudad.longitud)))) % n
+
+def buscarCiudadOrigen(listaCiudades, ciudadOrigen, n):
     for i in range(n):
-        if(len(listaCiudades[(funcionHash(vuelo, n) + i) % n]) == 0):
+        if(len(listaCiudades[(funcionHash(ciudadOrigen, n) + i) % n]) == 0):
             return -1
-        if(listaCiudades[(funcionHash(vuelo, n) + i) % n][0].nombre == vuelo[0]):
-            return i
+        if(listaCiudades[(funcionHash(ciudadOrigen, n) + i) % n][0].nombre == ciudadOrigen.nombre):
+            return i + funcionHash(ciudadOrigen, n)
     return -1
+
 def buscarCiudad(listaCiudades, nombreCiudad):
-    #print("Buscando " + nombreCiudad + " con " + str(len(listaCiudades)))
-    #print(listaCiudades)
     for ciudadesDestino in listaCiudades:
         if(ciudadesDestino.nombre == nombreCiudad):
             return True
     return False
 
-def escribirDestinos():    
+def escribirDestinos(n):    
     with open('../data/dataset1.csv', 'r') as file:
         reader = csv.reader(file)
-        #Tenemos que crear un arreglo que en vuelos[origen] = {vuelosDestino}
-        #posteriormente recorremos el arreglo y escribimos todos los vuelos destino en los origen.json
         listaCiudades = [list()]
-        n = 71
+        
         for i in range(n):
             listaCiudades.append(list())
         #Vamos a guardar en el arreglo los n aereopuertos distintos con sus vuelos posibles.
         i = 0;
         for vuelo in reader:
-            if(len(listaCiudades[0]) > 0):
-                print("Mexico0 se ha creado")
-            if(len(listaCiudades[1]) > 0):
-                print("Mexico1 se ha creado")
             if(i > 0):
-                index = buscarCiudadOrigen(listaCiudades, vuelo, n)
+                ciudadOrigen = Ciudades(vuelo[0], vuelo[2], vuelo[3])
+                index = buscarCiudadOrigen(listaCiudades, ciudadOrigen, n)
                 if(index == -1):
                     for j in range(n):
-                        if(len(listaCiudades[(funcionHash(vuelo, n) + j) % n]) == 0):
-                            listaCiudades[(funcionHash(vuelo, n) + j) % n].append(Ciudades(vuelo[0], float(vuelo[2]), float(vuelo[3])))
-                            index = (funcionHash(vuelo, n) + j) % n
-                            print("La ciudad " + vuelo[0] + " se esta creando, hk: " + str(index))    
+                        if(len(listaCiudades[(funcionHash(ciudadOrigen, n) + j) % n]) == 0):
+                            listaCiudades[(funcionHash(ciudadOrigen, n) + j) % n].append(Ciudades(ciudadOrigen.nombre, float(ciudadOrigen.latitud), float(ciudadOrigen.longitud)))
+                            index = (funcionHash(ciudadOrigen, n) + j) % n
                             break
-                    print("Acabamos")
                 if(not buscarCiudad(listaCiudades[index], vuelo[1])):
                     listaCiudades[index].append(Ciudades(vuelo[1], float(vuelo[4]), float(vuelo[5])))
             i += 1
+            
         for i in range(n):
             if(len(listaCiudades[i]) > 0):
-                print("La ciudad " + listaCiudades[i][0].nombre + " en el indice " + str(i))
-        #row[0] contiene cdO, [1] cdD, [2] origenLat, [3] longOrigen, [4] latDest, [5] longDest
-        
+                with open(listaCiudades[i][0].nombre + '.json', 'w') as archivo:
+                    json.dump(listaCiudades[i], archivo, indent=4, cls=CiudadesEncoder)
         #Abrimos el json con el codigo del origen:
         #escribimos un array con los destinos posibles en objeto {cod, long, lat}
         #El primer elemento del arreglo sera la informacion del mismo areopuerto
         
-def leerDestinos():
-    print("Leyendo los destinos")
-    ciudadOrigen = "Aca"
+def leerDestinos(ciudadOrigen):
     with open(ciudadOrigen + ".json") as archivo:
-        datos = json.load(archivo)
-    print(datos)
-    #Leeremos los destinos y los guardamos en un arreglo
-    
-def guardarClima():
-    print("Guardando el clima en una cache")
+        destinos = json.load(archivo)
+        for destino in destinos:
+            if(destino[0] != ciudadOrigen):
+                print(destino)
+        return destinos
+def cargarDatos():
+    with open("historial.json") as archivo:
+        historial = json.load(archivo)
+        return historial
+def mostrarClima(datosOrigen, datosDestino):
+    print(datosOrigen)
+    print(datosDestino)
+    historial = open("historial.txt", 'a')
+    historial.write(str(datosOrigen) + str(datosDestino) + "\n")
+    historial.close()
     #Crearemos un arreglo similar al de donde guardamos los vuelos pero ahora con el clima de cada aereopuerto en tupla con el tiempo cuando lo consultamos, con este tiempo podremos saber si necesitamos volver a calcular o no.
-
-def main():
-    escribirDestinos()
+def leerHistorial():
+    historial = open("historial.txt", 'r')
+    print(historial.read())
+    historial.close()
     
-    #url = "https://api.openweathermap.org/data/2.5/weather?lat=19.3371&lon=-99.566&appid=9d92b9e2262e46e5b34601d6f706cf43"
-    #f = urllib.request.urlopen(url,timeout=30)
-    #djson = json.loads(f.read())
-    #print(djson)
-    #print(djson["weather"][0]["description"])
+def main():
+    n = 71
+    escribirDestinos(n)
+    historialClima = [list()]
+    for i in range(n):
+        historialClima.append(list())
+    ciudadOrigen = Ciudades("MEX", 19.4363, -99.0721)
+    api = "9d92b9e2262e46e5b34601d6f706cf43"
+    if(len(historialClima[funcionHash(ciudadOrigen, n)]) > 0 and (datetime.now() - historialClima[funcionHash(ciudadOrigen, n)][1]) > datetime.timedelta(hours=0, minutes=30)):
+        djsonOrigen = historialClima[funcionHash(ciudadOrigen, n)][0]
+    else:
+        url = "https://api.openweathermap.org/data/2.5/weather?lat=" + str(ciudadOrigen.latitud) + "&lon=" + str(ciudadOrigen.longitud) + "&appid=" + api
+        f = urllib.request.urlopen(url,timeout=30)
+        djsonOrigen = json.loads(f.read())
+        historialClima[funcionHash(ciudadOrigen, n)].append(djsonOrigen)
+        historialClima[funcionHash(ciudadOrigen, n)].append(datetime.now())
 
+    destinos = leerDestinos(ciudadOrigen.nombre)
+    ciudadDestinoCodigo = input()
+    
+    
+    longitudDestino = 0.0
+    latitudDestino = 0.0
+    for destino in destinos:
+        if (destino[0] == ciudadDestinoCodigo):
+            latitudDestino = destino[1]
+            longitudDestino = destino[2]
+    ciudadDestino = Ciudades(ciudadDestinoCodigo, latitudDestino, longitudDestino)
+    if(len(historialClima[funcionHash(ciudadDestino, n)]) > 0 and (datetime.now() - historialClima[funcionHash(ciudadDestino, n)][1]) > datetime.timedelta(hours=0, minutes=30)):
+        djsonDestino = historialClima[funcionHash(ciudadOrigen, n)][0]
+    else:
+        url = "https://api.openweathermap.org/data/2.5/weather?lat=" + str(latitudDestino) + "&lon=" + str(longitudDestino) + "&appid=" + api
+        f = urllib.request.urlopen(url,timeout=30)
+        djsonDestino = json.loads(f.read())
+        historialClima[funcionHash(ciudadOrigen, n)].append(djsonOrigen)
+        historialClima[funcionHash(ciudadOrigen, n)].append(datetime.now())
+    mostrarClima(djsonOrigen, djsonDestino)
+    leerHistorial()
+    #print(djson["weather"][0]["description"])
 main()
--- src/testJson.py
+++ src/testJson.py
@@ -10,22 +10,32 @@ class Ciudades:
         self.nombre = nombre
         self.longitud = longitud
         self.latitud = latitud
-
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, 
+            sort_keys=True, indent=4)
 data = {}
+
 acapulco = Ciudades("ACA", 99, -10)
 mexico = Ciudades("MEX", 10, 77)
 monterrey = Ciudades("MTY", 10, 77)
+datas = {"ciudad":{acapulco, mexico, monterrey}}
 data['ciudad'] = []
 data['ciudad'].append(acapulco)
 data['ciudad'].append(mexico)
 data['ciudad'].append(monterrey)
 ciudades = [acapulco, mexico, monterrey]
+array3 = {'ciudad':[acapulco, mexico, monterrey]}
+#print(json.dumps(datas))
 #with open(acapulco.nombre + '.json', 'w') as archivo:
-#    for ciudad in data['ciudad']:
-#        json.dump((ciudad).__dict__, archivo, indent=4)
+#    json.dump(data, archivo, indent=4, cls=ComplexEncoder)
+#    for ciudad in datas["ciudad"]:
+#        json.dump(ciudad.__dict__, archivo, indent=4)
     #for x in data['ciudad']:
     #    json.dump(ComplexEncoder().encode(x), archivo, indent=4)
 
-#with open("ACA.json") as archivo:
-#    datos = json.load(archivo)
-#    print(datos)
+with open("ACA.json") as archivo:
+    datos = json.load(archivo)
+    for d in datos:
+        print(d)
+ #   for d in datos['ciudad']:
+ #       print(d)
