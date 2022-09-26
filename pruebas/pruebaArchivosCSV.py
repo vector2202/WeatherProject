0b51e51d4bd75ef65855ea1160900b1d25ad7533
Victor Torres <victor.torrest@ciencias.unam.mx>
2022-09-26 15:54:42 -0500

    Asserts en archivos csv

--- pruebas/pruebaArchivosCSV.py
+++ pruebas/pruebaArchivosCSV.py
@@ -1,2 +1,7 @@
-from src import archivosCSV
+import archivosCSV
 
+def main():
+    #assert(archivosCSV.revisarCsv())
+    #assert(archivosCSV.escribirDestinos("dataset1.csv", 71))
+    assert(archivosCSV.leerDestinos("MEX"))
+main()
