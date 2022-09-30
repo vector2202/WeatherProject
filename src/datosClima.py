4f5f806b5025867fde636b989ecdf6d169c7e670
DiegoCastroRendon <diego.castro.rendon@ciencias.unam.mx>
2022-09-30 17:37:56 -0500

    borrando comentarios basura

--- src/datosClima.py
+++ src/datosClima.py
@@ -13,8 +13,6 @@ class DatosClima:
                                                - 273))
         self.icono = "http://openweathermap.org/img/w/" +\
             str(datosJSON['weather'][0]['icon']) + ".png"
-        #informacion += "Amanecer: " + str(datosJSON['sys']['sunrise']) + "\n"
-        #informacion += "Atardecer: " + str(datosJSON['sys']['sunset']) + "\n"
 
     def __str__(self) -> str:
         return self.ubicacion + '\n' + self.descripcion +\
--- src/interfaz.py
+++ src/interfaz.py
@@ -11,42 +11,33 @@ from src.cacheClima import CacheClima
 class Interfaz(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
-        self.grid() # Esto es la cuadricula para poder poner los textos y opciones.
+        self.grid()
         self.inicializarJsons()
         self.createWidgets()
         self.master = master
         self.master.minsize(400,250)
         self.api = "9d92b9e2262e46e5b34601d6f706cf43"
-        #self.owm = OpenWeatherMap()
-        #self.temp_icon = OWIconLabel(self, weather_icon = owm.get_icon_data)
-        #self.temp_icon.grid(row=0, column=0)
+        
     
     def createWidgets(self):
-        self.historial = ttk.Button(self, text="Historial", command=self.mostrarHistorial)#boton de historial para mostrar lo consultado
-        #self.historial.pack(side=LEFT, padx=20, pady=20)
+        self.historial = ttk.Button(self, text="Historial", command=self.mostrarHistorial)
         self.historial.place(x=20,y=20)
 
-        #Seccion de la API
         self.apiText = ttk.Label(text="Escriba la llave:")
         self.apiText.place(x=200,y=20)
-        self.apiLlave = ttk.Entry()#caja de texto para escribir la llave de la API
+        self.apiLlave = ttk.Entry()
         self.apiLlave.place(x=320, y=20, width=60)
         self.apiBoton = ttk.Button(self, text="API", command=self.registrarAPI)
         self.apiBoton.place(x=390, y=20)
-        #self.apiBoton.pack(side=RIGHT, padx=290, pady=20)
         
-        #Boton para moestrar los climas
         self.consultar = ttk.Button(self, text="Consultar", command=self.destinoElegido)
         self.consultar.place(x=390,y=130)
-        #self.consultar.pack(side=RIGHT,padx=390,pady=50)
 
-        #Label para cd Origen y Destino
         self.ciudadOrigenLabel = Label(self, text="Ciudad de origen")
         self.ciudadOrigenLabel.place(x=20,y=100)
         self.ciudadDestinoLabel = Label(self, text="Ciudad de destino:")
         self.ciudadDestinoLabel.place(x=200, y=100) 
 
-        #Arreglos de informacion de origenes y destinos
         self.optionVarOrigen = tk.StringVar(self)
         self.origen = tk.OptionMenu(self, self.optionVarOrigen,\
                                     self.listaOrigenes[0], *self.listaOrigenes,\
@@ -62,7 +53,6 @@ class Interfaz(tk.Frame):
         self.optionVarDestino.set("Selecciona el destino:")
 
         
-        #Informacion que se va a mostrar
         self.climaOrigenDatos = StringVar()
         self.climaDestinoDatos = StringVar()
         self.climaOrigenDatos.set("")
@@ -72,19 +62,7 @@ class Interfaz(tk.Frame):
         self.climaDestino = ttk.Label(self, textvariable=self.climaDestinoDatos)
         self.climaDestino.place(x=200, y=170)
 
-        #iconURL = "http://openweathermap.org/img/w/" + str(datos['weather'][0]['icon']) + ".png"
-        #self.labelMostrarClima = ttk.Label(self, text= mostrarClima)
-        #self.labelMostrarClima.place(x=20, y=190)
-
-        #imageUrl = "http://openweathermap.org/img/w/.png"
-        #u = urlopen(imageUrl)
-        #rawData = u.read()
-        #u.close()
-        #photo = ImageTk.PhotoImage(data=rawData)
-        #label = tk.Label(image=photo)
-        #label.image = photo
-        #label.pack()
-        #Boton para salir del programa
+        
         self.buttonQuit = ttk.Button(self, text="Salir del programa",\
                                      command=self.saluda)
         self.buttonQuit.pack(padx=200, pady=350)
@@ -94,25 +72,23 @@ class Interfaz(tk.Frame):
     
     def inicializarJsons(self):
         nombreArchivoCSV = "dataset1.csv"
-        #Tamaño de la tabla de dispersion
         tamañoDiccionario = 71
         self.listaOrigenes = []
         self.listaDestinos = []
         try:
-            #Revisamos si el archivo CSV es correcto
             revisarCsv(nombreArchivoCSV)
         except Exception as exception:
             sys.exit(str(exception))
-        #Escribimos los JSON de cada cd Origen
+
         listaAeropuertos = escribirDestinos(nombreArchivoCSV, tamañoDiccionario)
-        #Inicializamos la lista de ciudades origen
+        
         self.listaOrigenes = listaAeropuertos.obtenerNombres()
-        #Creamos la cache de los climas
+        
         self.cache = CacheClima(tamañoDiccionario)
 
     def origenElegido(self, *args):
         nombreAeropuertoOrigen = self.optionVarOrigen.get()
-        self.destinosDisponibles = leerDestinos(nombreAeropuertoOrigen)#try excepto en lugar de un if none
+        self.destinosDisponibles = leerDestinos(nombreAeropuertoOrigen)
         if(self.destinosDisponibles != None):
             self.aeropuertoOrigen = Aeropuerto\
                 (self.destinosDisponibles[0][0],\
@@ -151,7 +127,7 @@ class Interfaz(tk.Frame):
         return False
 
     def consultarVuelo(self):
-        #definir los Json
+        
         if(self.request):
             self.mostrarClima(self.cache.obtenerClima(self.aeropuertoOrigen),\
                               self.cache.obtenerClima(self.aeropuertoDestino))
@@ -161,7 +137,6 @@ class Interfaz(tk.Frame):
     def mostrarClima(self, datosOrigen, datosDestino):
         self.climaOrigenDatos.set(datosOrigen)
         self.climaDestinoDatos.set(datosDestino)
-        #Registramos el vuelo en un txt
         archivoHistorial = open("datos/historial.txt", 'a')
         archivoHistorial.write(convertirVuelo(datosOrigen, datosDestino))
         archivoHistorial.close()
@@ -179,4 +154,4 @@ class Interfaz(tk.Frame):
     def registrarAPI(self):
         self.api = self.apiLlave.get()
         self.cache.actualizarAPI(self.api)
-        print(str(self.api))#sobreescribir el archivo de la api y la variable api
+        print(str(self.api))
