79a4c4d771bea5fb4785b8bd80888ebd2615b3f7
DiegoCastroRendon <diego.castro.rendon@ciencias.unam.mx>
2022-09-30 17:22:32 -0500

    Borrando comentarios basura

--- src/archivosCSV.py
+++ src/archivosCSV.py
@@ -24,9 +24,7 @@ def escribirDestinos(nombreArchivo, tamañoDiccionario):
     with open('datos/' + nombreArchivo, 'r') as archivo:
         next(archivo)
         vuelos = csv.reader(archivo)
-        
         lista = ListaDeAeropuertos(tamañoDiccionario)
-              
         for vuelo in vuelos:
             lista.procesarVuelo(vuelo)
         
